d923f63b554edcc14fa7b3a8e049540129b007f1
ManuelMartinDev <manuelmartindev@outlook.com>
2022-10-09 17:49:27 +0200

    initial

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script >
+export default {
+  data() {
+    return {
+      count: 10
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>Hi, count is {{count}}</h1>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
---
+++ src/components/HelloWorldComp.vue
@@ -0,0 +1,15 @@
+<script setup >
+import { ref } from "vue"
+const count = ref(10)
+
+</script>
+
+<template>
+    <h1>Hi, count is {{count}}</h1>
+</template>
+
+<style scoped>
+.read-the-docs {
+    color: #888;
+}
+</style>
